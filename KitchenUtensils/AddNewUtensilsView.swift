bbb6313bb1cd2905d5d0e941c45ad711527f36bd
MonicaKennedy92 <monicakennedyofficial@gmail.com>
2025-08-30 12:56:24 -0600

    Created AddNewUtensilsView and Setup Preview for the UtensilListView with the sample Data and the EmptyView , added Add button on the toolbar rightside when it is clicked present the AddNewUtensilsView

---
+++ KitchenUtensils/AddNewUtensilsView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewUtensilsView.swift
+//  KitchenUtensils
+//
+//  Created by Monica Kennedy on 2025-08-30.
+//
+
+import SwiftUI
+
+struct AddNewUtensilsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewUtensilsView()
+}
