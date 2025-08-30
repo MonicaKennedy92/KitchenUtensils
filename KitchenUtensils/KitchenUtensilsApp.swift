ac536da9eb3a5a4ba73a7c382b8b05a599d2252c
MonicaKennedy92 <monicakennedyofficial@gmail.com>
2025-08-30 12:13:05 -0600

    Initial Commit

---
+++ KitchenUtensils/KitchenUtensilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenUtensilsApp.swift
+//  KitchenUtensils
+//
+//  Created by Monica Kennedy on 2025-08-30.
+//
+
+import SwiftUI
+
+@main
+struct KitchenUtensilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
